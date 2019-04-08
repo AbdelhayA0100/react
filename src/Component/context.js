309bcc511e3c21b2020003e25071a3850efb4b21
Abdelhay bensassi <abdelhaybensassi.m@gmail.com>
2019-04-08 16:34:53 +0100

    done

--- src/Component/context.js
+++ src/Component/context.js
@@ -8,7 +8,7 @@ export  class Provider extends Component {
     state={
         users:[{id:1 ,name:"Mohammed",Email:"Mohammed@gmail.com"}
         ,
-        {id:2 ,name:"Kamal",Email:"Kamal@gmail.com"}]
+        {id:2 ,name:"Kamal",Email:"Kamal@gmail.com"},{id:2 ,name:"Kamal",Email:"Kamal@gmail.com"}]
     }
   render() {
 
